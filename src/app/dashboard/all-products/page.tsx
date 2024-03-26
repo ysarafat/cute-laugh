739d0cb30ca66e6ea2300c2abf46a06785f90c7c
Yeasir Arafat <ysarafat.bd@gmail.com>
2024-03-26 16:07:43 +0600

    all product table

--- src/app/dashboard/all-products/page.tsx
+++ src/app/dashboard/all-products/page.tsx
@@ -1,7 +1,45 @@
-const AllProducts = () => {
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { TProduct } from "@/types/product";
+
+const AllProducts = async () => {
+  const res = await fetch(
+    "https://cute-laugh-backend.vercel.app/api/products",
+    {
+      cache: "no-store",
+    }
+  );
+  const { data: product } = await res.json();
   return (
     <div>
-      <h1>all products</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">SL</TableHead>
+            <TableHead>Item</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Ratings</TableHead>
+            <TableHead className="text-right">Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {product?.map((item: TProduct, index: number) => (
+            <TableRow key={item._id}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell>{item.title}</TableCell>
+              <TableCell>{item.category}</TableCell>
+              <TableCell>{item.ratings}</TableCell>
+              <TableCell className="text-right">${item.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import MobileSidebar from "@/components/dashboard/MobileSidebar";
 import Sidebar from "@/components/dashboard/Sidebar";
 import { ReactNode } from "react";
 
@@ -7,7 +8,11 @@ const layout = ({ children }: { children: ReactNode }) => {
       <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 bg-gray-900">
         <Sidebar />
       </div>
-      <main className="md:pl-80 md:py-10 p-4">{children}</main>
+
+      <main className="md:pl-80 md:py-10 p-4">
+        <MobileSidebar />
+        {children}
+      </main>
     </div>
   );
 };
